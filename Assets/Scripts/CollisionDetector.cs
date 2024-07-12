209348569c7eef8d4e2dcd15ae36599bce0692db
이지연 <a01092857503@gmail.com>
2024-07-12 18:01:34 +0900

    [Updata]가시함정 넉백기능 추가

--- Assets/Scripts/CollisionDetector.cs
+++ Assets/Scripts/CollisionDetector.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aadc7385bd4c5fcd0a63de554e1efb205efd1416c066cd6a19c0cfe25ec0ce1
-size 1063
+oid sha256:3aadc957f5ba2cbb70250738ab94a672cbc6d8deabb604f8216e23dea880513d
+size 743
---
+++ Assets/Scripts/ThornTrapKnockback.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef8b7865b8c7d165c5b2783b8e3a5ce3dcc76a188bc9549df3c7fbd64beeffae
+size 958
