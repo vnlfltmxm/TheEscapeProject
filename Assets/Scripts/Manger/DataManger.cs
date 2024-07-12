416028cf34c4ebb963b2fe65f6bb20b4a79c7794
박상욱 <vnlfltmxm@gmail.com>
2024-07-12 18:02:28 +0900

    [fix] 데이터 매니저 코드 수정

--- Assets/Scripts/Manger/DataManger.cs
+++ Assets/Scripts/Manger/DataManger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2c9b626e40738d7e69327a052a8e72424419882ee00fefe2afb64e258c3f025
-size 10440
+oid sha256:bfc29a31164177283ec07ae3ad45d7dae3e1a3b608f0629dbd552b241e5d57dc
+size 10635
