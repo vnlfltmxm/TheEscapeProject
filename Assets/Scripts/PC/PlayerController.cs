9c6bf59c77b217aad1143b9a17d5a9c688abdea2
우종현 <whdgus616@gmail.com>
2024-07-12 18:04:03 +0900

    [feat] 플레이어 사다리 수정 중

--- Assets/Scripts/PC/PlayerController.cs
+++ Assets/Scripts/PC/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f63847a465342dd1d372ebe1fea056aff747fcee68db9c6f0b00b514522d9182
+oid sha256:a0518d3675e3f04295335e324d0feee754aee9abbe5729c3e32347a5ccb5b06e
 size 3586
