ecb864173de72101cfa268d37de27dd6379ff840
박상욱 <vnlfltmxm@gmail.com>
2024-07-12 16:30:35 +0900

    [Add] 일반 몬스터 죽는 기능 추가
    이펙트는 추가 안함

--- Assets/Scripts/Enemy/BossController.cs
+++ Assets/Scripts/Enemy/BossController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf27a814f9ab98b0ef0b2dc61a4d03b271176a3b64a98272db3f0d6e6ca513c5
-size 2856
+oid sha256:90da47d6cfdd43c9f530c542786d9a519b8d0a6fa7f21fdb0d74d3134a292775
+size 3032
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f5226f5bb639a5cedb9b950a5ce0bc57aa4467747c3cd1ef2e26f1412eabb0f
-size 397
+oid sha256:6a8b1941021881471d23af6a1b241f4689a46b4dfe41711e54e7921ae5e93aea
+size 990
