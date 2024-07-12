414c3c35794e173324415d5e6d6a040201dfe26e
박상욱 <vnlfltmxm@gmail.com>
2024-07-12 15:51:27 +0900

    [Add] 보스 이속감소 스킬 추가

--- Assets/Scripts/Enemy/BossController.cs
+++ Assets/Scripts/Enemy/BossController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3c8f09a3657a13b08f196cfcded70798082444bc98b17ac85614a9cabc526c4
-size 1631
+oid sha256:cf27a814f9ab98b0ef0b2dc61a4d03b271176a3b64a98272db3f0d6e6ca513c5
+size 2856
