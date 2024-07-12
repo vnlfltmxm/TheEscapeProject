5f4deb6903952c9c0bf47a5d92d3366bf5a1f488
jihe05 <163379277+jihe05@users.noreply.github.com>
2024-07-12 15:11:54 +0900

    [Add] 르랩 옵브젝트 움직임

---
+++ Assets/Scripts/TrapRoom/MoveTrap.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c78f6779d76dceda3b04a569ea376c131cae6c266d46355961fe013a64f20dc
+size 1957
---
+++ Assets/Scripts/TrapRoom/RotatingTrap.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6de416d56d9bdf35919549b7cc4007ff3a0ccb21726764e02fd3f11c8ad265a8
+size 2123
