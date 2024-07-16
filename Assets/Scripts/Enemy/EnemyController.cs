39dd3e0238f34baefb2bc8b067c413e84b0210f3
박상욱 <vnlfltmxm@gmail.com>
2024-07-16 16:56:10 +0900

    [fix] 코드정리

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a9dd448acf0bb13a2e996569f3b0e2de1d140ae5655e6826813d185d2a678f5
-size 2379
+oid sha256:1c7f1b925acdefc78a36bbf017e2aa21f355c06b68b5b7066478e8301f3b0484
+size 2389
