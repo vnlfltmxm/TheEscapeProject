75f219f7da72c176c744caa9ff2222144086a988
jihe05 <163379277+jihe05@users.noreply.github.com>
2024-07-16 15:34:55 +0900

    [Fead] 트랩 룸 배치

--- Assets/Scripts/TrapRoom/RoomDoor.cs
+++ Assets/Scripts/TrapRoom/RoomDoor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f5d4e59d34bfe5ac06ed46142f6d7b0fc0a17bcad6ba78da7be811aba4389b7
-size 1425
+oid sha256:b461f66213b5bcad200295ce5c4b8127b8eb5096753c747a203dfe787060f4c0
+size 1394
