79acd96d360e41afff8144d0d2efedba495a53f7
우종현 <whdgus616@gmail.com>
2024-07-17 13:26:18 +0900

    [feat] Monster, Skill data 수정

--- Assets/Scripts/PC/PlayerController.cs
+++ Assets/Scripts/PC/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c75117509077fb39278b62226e739e27c28b20cbb380ea9c615136737406f9c8
-size 4482
+oid sha256:6531a799bbf4c6a74bc1e0e5f837e456dde099f8b87c8a5d1d802e29fc3b760a
+size 4723
