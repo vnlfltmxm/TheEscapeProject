662fc561ada68af8f5b11408db8128eb79724ee3
박상욱 <vnlfltmxm@gmail.com>
2024-07-17 13:22:49 +0900

    [fix]반사 굴절 오브젝트 수정

---
+++ Assets/Scripts/Ray/ReflectObj.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6578362bcb9e77ec1d3a77922d520615c2a6f7a4a07f7092168ac1a9dae62427
+size 1139
--- Assets/Scripts/Ray/reflect.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6596d1fe2a9f63b417a23193967d95fb11e9a6d65cab64e8e738d3452d30a332
-size 309
