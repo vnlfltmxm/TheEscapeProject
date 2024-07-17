1591d8806d27827723a3024ad612bad69a5ad0ff
이지연 <a01092857503@gmail.com>
2024-07-17 21:11:33 +0900

    [Add] Tag, Layer 추가

--- Assets/Scripts/JiYeon/FlashBang.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c9b502066f1f0ad069d661e5e9ac2cf6c011f4980d6ba67f0bddb61002a757c7
-size 132
---
+++ Assets/Scripts/JiYeon/PlaneMoving.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:88dd0e51968c18f958bf2022c2b31e2c6a332b6e099068fa4cd6a38c40240ed4
+size 2394
