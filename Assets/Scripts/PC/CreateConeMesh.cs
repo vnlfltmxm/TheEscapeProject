ff4c6551e7923a56f3cb46b1ab4b7dcf927df866
우종현 <whdgus616@gmail.com>
2024-07-17 21:01:16 +0900

    [feat] 손전등 지형 버그 수정

--- Assets/Scripts/PC/CreateConeMesh.cs
+++ Assets/Scripts/PC/CreateConeMesh.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc0cd4c53c12f4ee6d0c5a29b9b5dccf504a0187b1920181d9fc71a82f5c5ffa
-size 7000
+oid sha256:d9cf88620ce5c080c97fa4d88f01ac35a7fafd46279b96934a697b8ff29dd5a9
+size 8359
