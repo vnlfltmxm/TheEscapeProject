c35e06b5ad1ff5711f7900208520721e9d57dadc
우종현 <whdgus616@gmail.com>
2024-07-17 16:37:33 +0900

    [update] GameScene 추가

--- Assets/Scripts/PC/CreateConeMesh.cs
+++ Assets/Scripts/PC/CreateConeMesh.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75d57c650019f1042c616741a808e311eb8145edc60e60a6b18503dc66977ff1
-size 5030
+oid sha256:bede3b4ce5f851e89be8ada6c5a72982fa988ca3c90bbbb054ff6af39558bd9a
+size 6546
--- Assets/Scripts/PC/FlashlightController.cs
+++ Assets/Scripts/PC/FlashlightController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04a6c43f788c1fe5598aa6174cceb24219d79b7d0ed30c0722308421a299eb61
-size 2783
+oid sha256:8f7718465f79d8e96609c15d0c94c196c3aba28080e28162ba3b04a9e609fc90
+size 2933
