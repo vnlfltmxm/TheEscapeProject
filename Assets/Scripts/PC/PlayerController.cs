c2650d6d253e190c33f168246ab61955d54e0f82
우종현 <whdgus616@gmail.com>
2024-07-18 17:34:27 +0900

    [fix] 버그수정

--- Assets/Scripts/PC/PlayerController.cs
+++ Assets/Scripts/PC/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f18c36fa5cf1f39240d648d91c08ef7a761ee1ebd6d5813c48b059c703043180
-size 12341
+oid sha256:a8875d6de5206f9b6edc82a261fb05338441c39364d23f98f66dd38ec212f08b
+size 12522
--- Assets/Scripts/TrapRoom/refractionRotating.cs
+++ Assets/Scripts/TrapRoom/refractionRotating.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccaa603a036553751ed1a3cc9842586051cf37a86906723c1d76d79b96b3db76
-size 3285
+oid sha256:a0e2184b642f96a6c024e3a1126b4ce61bff54e9db8ee0107592d9ed07fc5229
+size 3225
