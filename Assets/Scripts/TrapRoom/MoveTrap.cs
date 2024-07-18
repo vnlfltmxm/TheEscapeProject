4cfd78d25fb91a4c3a4e1f94d2c23b6232b7587f
jihe05 <163379277+jihe05@users.noreply.github.com>
2024-07-18 10:16:32 +0900

    [fead] 회전트랩, 반원Move트랩, 직선Move트랩 프리팹화

--- Assets/Scripts/TrapRoom/MoveTrap.cs
+++ Assets/Scripts/TrapRoom/MoveTrap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:855753f17563b55c0f55ed803ce35bc436af59deeafc0813abd5cff22a5bf5a1
-size 1675
+oid sha256:05ab03ccfa00c5218af71c50dd999b89f43315bc7dab0236f80c7dd04f039ec5
+size 1672
