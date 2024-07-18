7cc35ac5ed63c37fe4e5974643326b29cad2b07e
우종현 <whdgus616@gmail.com>
2024-07-18 16:02:21 +0900

    [fix] Player Jump 물리 수정

--- Assets/Scripts/PC/PlayerController.cs
+++ Assets/Scripts/PC/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8114166a009a4ac24c7aceca70bcc440123a122516cd68e93690d4cec56835c
-size 12015
+oid sha256:f18c36fa5cf1f39240d648d91c08ef7a761ee1ebd6d5813c48b059c703043180
+size 12341
