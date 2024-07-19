3d3598f42c98625b44d77574a260d3b76e44efa5
박상욱 <vnlfltmxm@gmail.com>
2024-07-19 16:58:30 +0900

    push용

--- Assets/Scripts/TrapRoom/MoveTrap.cs
+++ Assets/Scripts/TrapRoom/MoveTrap.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a52a75f4062880167d84c4250e954f83a04cd84d25250e2e4012c30fd21b92e2
-size 1901
+oid sha256:4ffe4318b03bba2b94ea5f8395364e0d9c1a9abb80584b2544f16f5c651737ac
+size 1988
