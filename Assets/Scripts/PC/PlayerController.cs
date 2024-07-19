3a238746a3f1b49cec6cd25c987f5a441e3ee1d5
우종현 <whdgus616@gmail.com>
2024-07-19 10:13:59 +0900

    [fix] trap에 충돌 시 플레이어 축 고정

--- Assets/Scripts/PC/PlayerController.cs
+++ Assets/Scripts/PC/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8875d6de5206f9b6edc82a261fb05338441c39364d23f98f66dd38ec212f08b
-size 12522
+oid sha256:85b81e088282be03ff28c5e3c8ddbe0bdaaec78078b812e5638c7671f2276db0
+size 12670
