02eef7859c2c53f3688c3fe82ce035a81e88edd2
박상욱 <vnlfltmxm@gmail.com>
2024-07-19 14:30:40 +0900

    [fix] 반사와 굴절 오브젝트에 라인이 이상하게 그려지는 문제 수정

--- Assets/Scripts/Ray/ShotRay.cs
+++ Assets/Scripts/Ray/ShotRay.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:520ddcfaea25aa79ee045746e5b7bb59bb70de3c796e481899c663ee9d6c68d5
-size 5889
+oid sha256:e585ed08434635133f515c9f26f400420f13bbd1deba671955a24a5b48bb9efa
+size 5698
